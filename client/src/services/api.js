188fa6298283ae07a30e5ab983203c0c932a2d71
indonasiasv2-rgb <indonasiasv2@gmail.com>
2026-04-25 12:32:06 +0530

    Updated API base URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000"
+  baseURL: "https://myblogs-r30i.onrender.com"
 });
 
 export default API;
